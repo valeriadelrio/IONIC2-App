53f8367aadcb3be205e160d8e8bc2d8cb474fce4
valeriadelrio <valeriadlrio@gmail.com>
2017-08-21 12:06:11 -0300

    Creación de services y clasesa

---
+++ src/app/services/lista-deseos.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Lista} from '../clases/listas';
+
+@Injectable()
+export class ListaDeseosService {
+
+  listas: Lista[]=[];
+
+  constructor() {
+
+    let lista1 = new Lista("Lista de supermercado");
+    let lista2 = new Lista("Juegos que deseo");
+    let lista3 = new Lista("Cosas de la universidad");
+
+    this.listas.push(lista1);
+    this.listas.push(lista2);
+    this.listas.push(lista3);
+
+    console.log("servicio inicializado");
+  }
+}
