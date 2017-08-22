b01817b76d647fd009706a123534e912e118891d
valeriadelrio <valeriadlrio@gmail.com>
2017-08-22 19:19:24 -0300

    Aplicacion terminada

--- src/pages/terminados/terminados.component.ts
+++ src/pages/terminados/terminados.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {ListaDeseosService} from '../../app/services/lista-deseos.service';
+
+import { NavController } from 'ionic-angular';
+import {DetalleComponent} from '../detalle/detalle.component';
 
 @Component({
   selector: 'app-terminados',
   templateUrl: 'terminados.component.html',
 })
 export class TerminadosComponent implements OnInit {
-  constructor() {  }
+  constructor( private _listaDeseosService: ListaDeseosService,
+                private navCtrl: NavController) {  }
 
   ngOnInit() {}
+
+  verDetalle(lista, i){
+    this.navCtrl.push( DetalleComponent, {lista, i});
+  }
 }
