867784f9cb22faaaecd046442541c6b05fd85719
valeriadelrio <valeriadlrio@gmail.com>
2017-08-22 17:25:49 -0300

    Almacenamiento LocalStorage

--- src/app/services/lista-deseos.service.ts
+++ src/app/services/lista-deseos.service.ts
@@ -7,15 +7,25 @@ export class ListaDeseosService {
   listas: Lista[]=[];
 
   constructor() {
-
-    let lista1 = new Lista("Lista de supermercado");
-    let lista2 = new Lista("Juegos que deseo");
-    let lista3 = new Lista("Cosas de la universidad");
-
-    this.listas.push(lista1);
-    this.listas.push(lista2);
-    this.listas.push(lista3);
+    this.cargarData();
 
     console.log("servicio inicializado");
+  }
+
+  actualizarData(){
+    localStorage.setItem("data", JSON.stringify(this.listas));
+  }
+
+  cargarData(){
+    if (localStorage.getItem("data")) {
+      this.listas = JSON.parse(localStorage.getItem("data"));
+    }
+
+  }
+
+  agregarLista(lista: Lista){
+    this.listas.push(lista);
+    this.actualizarData();
+
   }
 }
--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -48,7 +48,8 @@ export class AgregarComponent implements OnInit {
     }
     let lista = new Lista(this.nombreLista);
     lista.items = this.items;
-    this._listaDeseos.listas.push(lista);
+    this._listaDeseos.agregarLista(lista);
+    // this._listaDeseos.listas.push(lista);
     this.navCtrl.pop()
 
   }
